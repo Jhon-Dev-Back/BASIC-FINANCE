d6712e93ff2313668e0b493c7872a6de3d439923
Jhon-Dev-Back <jhonerrtes1@gmail.com>
2025-03-31 17:22:12 -0500

    script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ toggler.addEventListener("click", function() {
     const sidebar = document.querySelector("#sidebar");
     const mainContent = document.querySelector(".main");
 
-    // Togglear la clase "collapsed" en el sidebar
+  
     sidebar.classList.toggle("collapsed");
 
-    // Ajustar el margen izquierdo del main según si el sidebar está colapsado o no
+    
     if (sidebar.classList.contains("collapsed")) {
-        mainContent.style.marginLeft = "0"; // Sin margen cuando el sidebar está cerrado
+        mainContent.style.marginLeft = "0"; 
     } else {
-        mainContent.style.marginLeft = "264px"; // Con margen cuando el sidebar está abierto
+        mainContent.style.marginLeft = "264px"; 
     }
 });
